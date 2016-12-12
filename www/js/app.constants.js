0f9459c99802105292524a7178ae93fbe332b948
Patrick Williams <pdw0005@gmail.com>
2016-12-12 18:56:34 +0000

    changed base_server constant to reflect new hosting environment.

--- www/js/app.constants.js
+++ www/js/app.constants.js
@@ -1,3 +1,3 @@
-app.constant('BASE_SERVER', 'http://is.androidbox.tv');
+app.constant('BASE_SERVER', 'http://insurescan-api.us-east-1.elasticbeanstalk.com');
 app.constant('APP_DEBUG', true);
 //app.constant('BASE_SERVER', '');
