029e2fb5ffcd9a1bbb860b9f388f5fdcdab3858d
Patrick Williams <Patrick Williams>
2016-12-28 17:05:30 +0000

    INS-31

--- www/js/app.constants.js
+++ www/js/app.constants.js
@@ -1,3 +1,3 @@
-app.constant('BASE_SERVER', 'http://insurescan-api.us-east-1.elasticbeanstalk.com');
+app.constant('BASE_SERVER', 'http://dev-api.cloud-coverage.com');
 app.constant('APP_DEBUG', true);
 //app.constant('BASE_SERVER', '');
