a3e2332e4fe686d207c0b8722b6c7b313fcb45bd
patrick <rh.oa.developer@gmail.com>
2017-03-28 12:09:17 -0500

    INS-48, cleaned all unecessary files, will be merged into dev and superfluous branches will be deleted

--- www/app/components/newDriver/newDriver.edit.controller.js
+++
@@ -1,3 +0,0 @@
-ontrollers.controller('newDriverEditCtrl', ['BASE_SERVER', '$scope', '$state', '$stateParams', '$ionicSlideBoxDelegate', '$injector', '$timeout', function (baseUrl, $scope, $state, $stateParams, $ionicSlideBoxDelegate, $injector, $timeout){
-    //scope specific shit goes here
-}
